5888910ef0810936ff774f048696c1bf1d0fc864
João Pedro de Bem <jpdebem@gmail.com>
2023-09-20 10:59:12 -0300

    Subindo Arquivos

---
+++ ProgramaFaixaEtaria/001_ProgramaFaixaEtaria/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _001_ProgramaFaixaEtaria
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.Write("Qual sua idade? ");
+
+            int idade = int.Parse(Console.ReadLine());
+
+            Program programaIdade = new Program();
+            programaIdade.FaixaEtaria(idade);
+            Program programaContinuar = new Program();
+            programaContinuar.Continuar(idade);
+
+        }
+
+        public void FaixaEtaria(int idade)
+        {
+            if (idade < 20 && idade > 0)
+            {
+                if (idade == 1)
+                {
+                    Console.WriteLine($"\nCom {idade} ano de idade, você pertence ao grupo de indivíduos Jovens.");
+                }
+                else
+                {
+                    Console.WriteLine($"\nCom {idade} anos, você pertence ao grupo de indivíduos Jovens.");
+                }
+
+            }
+            else if (idade >= 20 && idade < 60)
+            {
+                Console.WriteLine($"\nVocê tem {idade} anos, já pertence ao grupo de indivíduos Adultos");
+            }
+            else if (idade >= 60 && idade < 110)
+            {
+                Console.WriteLine($"\nCom seus incríveis {idade} anos, você pertence ao grupo de indivíduos Idosos");
+            }
+            else if (idade > 110 && idade < 130)
+            {
+                Console.WriteLine($"\nNo auge dos seus {idade} anos você já se tornou uma relíquia em nossa história.");
+            }
+            else
+            {
+                Console.WriteLine("\nVocê inseriu uma idade inválida. Tente novamente!\n");
+                Main();
+            }
+
+        }
+
+        public void Continuar(int continuar)
+        {
+            Console.WriteLine("\n\nDeseja tentar uma outra idade?\n" + "[Digite 1 - Sim]\n" + "[Digite 2 - Não]\n");
+
+            continuar = int.Parse(Console.ReadLine());
+
+            if (continuar == 1)
+            {
+                Console.WriteLine("");
+                Main();
+            }
+            else if (continuar == 2)
+            {
+                Console.WriteLine("\n*-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-*\n");
+                Console.WriteLine("Programa encerrando em\n");
+                for (int i = 3; i > 0; i--)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(1000);
+                }
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida. Tente novamente!");
+                Continuar(continuar);
+            }
+
+        }
+
+
+    }
+
+}
---
+++ Repeticoes/005_Repeticoes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005_Repeticoes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            string[] palavras = { "Joao", "Pedro", "De", "Bem", "da", "Rosa" };
+
+
+            // while(){} - Execute enquanto for verdade
+            while (contador < 10)
+            {
+                Console.WriteLine("Executa enquanto for verdade - " + contador);
+                contador++;
+            }
+
+            // do{}while() - Execute ao menos 1 vez menos sendo mentira 
+            do 
+            {
+                Console.WriteLine("\nExecutar pelo menos uma vez mesmo sendo mentira\n");
+            }
+            while (1000 == 10);
+
+
+            // foreach(){} - Para cada palavra no array, repita 
+            foreach (string palavra in palavras) 
+            {
+                Console.WriteLine("foreach rodando - " + palavra);
+            }
+
+
+            Console.WriteLine("");
+            for(int novoContador = 0; novoContador < palavras.Length; novoContador++)
+            {
+                Console.WriteLine("for rodando em crescente - " + palavras[novoContador]); 
+                
+            }
+
+
+            Console.WriteLine("");
+            for (int novoContador2 = palavras.Length - 1 ; novoContador2 >= 0;  novoContador2--) 
+            {
+                Console.WriteLine("for rodando em decrescente - " + palavras[novoContador2]);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
